28ef4cb00742c265cdce781c9196e94c7b5dab05
viduminii <viduminiranasinghe1234@gmail.com>
2023-05-12 12:42:28 +0530

    Lab1 added

---
+++ Lab1/Question 01/Question 01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Username :");
+            String name = Console.ReadLine();
+            Console.Write("Enter Your Batch :");
+            String batch = Console.ReadLine();
+
+            Console.WriteLine("Enter Username :" + name);
+            Console.WriteLine("Enter your Batch :" + batch);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1/Question 02/Question 02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Radius :");
+            double radius = Convert.ToDouble(Console.ReadLine());
+            double calculation = radius * radius * Math.PI;
+            Console.WriteLine("Area: " + calculation);
+            Console.ReadKey();
+
+        }
+    }
+}
