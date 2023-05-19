6b021bd28d5c61487002433a387c13f0d48c4ba0
viduminii <viduminiranasinghe1234@gmail.com>
2023-05-19 12:42:29 +0530

    Lab2 added

---
+++ Lab2/Question 03/Question 03/Question 03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number :");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the second number :");
+            double num2 =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Sum: " + (num1 + num2));
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ Lab2/Question 04/Question 04/Question 04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your salary: ");
+            double salary = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter tax rate: ");
+            double tax = Convert.ToDouble(Console.ReadLine());
+            // double new_salary = salary * (100 - tax) / 100;
+            double new_salary = salary - (salary * (tax / 100));
+            Console.WriteLine("New Salary: " + new_salary);
+            Console.ReadKey();
+        }
+    }
+}
