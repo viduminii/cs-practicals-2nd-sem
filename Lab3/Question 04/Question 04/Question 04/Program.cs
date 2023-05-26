d78b2560f8dfe4beb4f6623c6a194565e3d90356
viduminii <viduminiranasinghe1234@gmail.com>
2023-05-26 12:42:29 +0530

    Lab3 added

---
+++ Lab3/Question 04/Question 04/Question 04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even number");
+            }
+            else
+            {
+                Console.WriteLine("Odd number");
+            }
+            Console.ReadKey();
+
+
+            
+
+        }
+    }
+}
