66baff1c23874a3d0e86f26adcdb5bef498db506
viduminii <viduminiranasinghe1234@gmail.com>
2023-06-02 12:42:30 +0530

    Lab4 added

---
+++ Lab4/Question 01/Question 01/Question 01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number one: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter number two: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            double sum = num1 + num2;
+            Console.WriteLine("sum: " +(num1 + num2));
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ Lab4/Question 02/Question 02/Question 02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = num1 + num2;
+            double subtraction = num1 + num2;
+            double multiplication = num1 * num2;
+            double division = num1 / num2;
+
+            Console.WriteLine("sum: " + sum);
+            Console.WriteLine("subtraction: " + subtraction);
+            Console.WriteLine("multiplication: " + multiplication);
+            Console.WriteLine("division: " + division);
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
